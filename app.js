f662109a062bfa44a2de7525ba77d991f1c8c7f4
jose <josep2789@icloud.com>
2020-03-30 02:44:42 -0700

    new additions and deletions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// app.set("view engine", "ejs");
+app.engine("view engine", "ejs");
 app.use(express.static("public")); 
 
 const request = require('request');
