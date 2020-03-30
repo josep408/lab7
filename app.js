ecdd963638c9b16de835f7001032aeeda10751bc
jose <josep2789@icloud.com>
2020-03-30 02:10:03 -0700

    test for bug fixes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function getImages(keyword){
     
     
     return new Promise( function(resolve, reject){
-        request('https://pixabay.com/api/?key=15451226-748d708b832ac6ef8a248a904'+keyword,
+        request('https://pixabay.com/api/?key=15451226-748d708b832ac6ef8a248a904&q='+keyword,
                  function (error, response, body) {
     
             if (!error && response.statusCode == 200  ) { // successful request
@@ -39,10 +39,6 @@ function getImages(keyword){
                  let parsedData = JSON.parse(body); //converting string to json 
                  
                  resolve(parsedData);
-                
-                // let randomIndex = Math.floor(Math.random() * parsedData.hits.length);
-                // // res.send(`<img src='${parsedData.hits[randomIndex].largeImageURL}'>`);
-                // res.render("index", {"images":parsedData});
                 
             } else {
                 reject(error);
@@ -56,15 +52,10 @@ function getImages(keyword){
     
 }
 
-
-// server Listener <-- DO NOT UNCOMMENT -->
-// app.listen("8081","127.0.0.1", function(){
-// 	console.dir("Express server is Running ...");
-// });
 // starting server <-- do not uncomment -->
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Express server is running...");
+	console.log("RUnning express Server ... ");
 });
 
 
